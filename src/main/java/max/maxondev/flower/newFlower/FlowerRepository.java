0622fba73038bddd190f0a296a3d47397d4a6a31
Максим Куценко <maxondevelop@Maksims-MacBook-Pro.local>
2023-11-11 13:22:49 +0200

    Data base added | commit

--- src/main/java/max/maxondev/flower/newFlower/FlowerRepository.java
+++
@@ -1,9 +0,0 @@
-package max.maxondev.flower.newFlower;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FlowerRepository extends JpaRepository<Flower, Long> {
-
-}
